3cc84d7e7b697ee3162d126838b15ce0f9bd068e
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-02-09 10:16:32 -0500

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1 = 31
+let num2 = 2
+
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3 = 32
+let num4 = 7
+
+
+let mod = num3 % num4 
+
+let max = Math.max(15, 16, 17, 18, 19, 20);
